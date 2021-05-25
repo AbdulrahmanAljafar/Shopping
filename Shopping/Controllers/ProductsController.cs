81cb16052dd09dda14ee7668461199a484a3c4f7
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-05-25 13:12:34 +0300

    W05D03

---
+++ Shopping/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shopping.Models;
+
+namespace Shopping.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index(int id = -1)
+        {
+            List<ProductModel> Product = new List<ProductModel>();
+            Product.Add(new ProductModel(1,"test",10.1f));
+            Product.Add(new ProductModel(2, "test2", 15.1f));
+            Product.Add(new ProductModel(3, "test3", 17.1f));
+
+            ViewData["Product"] = Product;
+            ViewData["id"] = id;
+
+            return View();
+        }
+
+        public IActionResult Details(int id = 1)
+        {
+            List<ProductModel> Product = new List<ProductModel>();
+            Product.Add(new ProductModel(1, "test", 10.1f));
+            Product.Add(new ProductModel(2, "test2", 15.1f));
+            Product.Add(new ProductModel(3, "test3", 17.1f));
+
+            ViewData["Product"] = Product;
+            ViewData["id"] = id;
+
+
+            return View();
+        }
+    }
+}
---
+++ Shopping/Models/ProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Models
+{
+    public class ProductModel
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public ProductModel(int id,string Name,float Price)
+        {
+            this.id = id;
+            this.Name = Name;
+            this.Price = Price;
+
+        }
+
+
+    }
+}
