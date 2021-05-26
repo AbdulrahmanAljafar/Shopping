42daaaad987407b633486e81712769000b331d9d
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-05-27 01:08:44 +0300

    edit List for the products

--- Shopping/Controllers/ProductsController.cs
+++ Shopping/Controllers/ProductsController.cs
@@ -9,13 +9,20 @@ namespace Shopping.Controllers
 {
     public class ProductsController : Controller
     {
+
+      private  List<ProductModel> Product = new List<ProductModel>() {
+       new ProductModel(1, "Espresso Machine", 300.1f, "https://www.ekuep.com/img/cms/Elementor%202/4740-thickbox_default%20(1).jpg"),
+       new ProductModel(2, "Black Coffee", 15.1f, "https://images.indianexpress.com/2015/10/black-coffee_759_petr-kratochvil_wikimedia-commons.jpg"),
+       new ProductModel (3, "Espresso", 17.1f, "https://www.illy.com/content/dam/channels/website/consumer/global/recipies/Doppio-tazza-media_mobile.jpg"),
+       new ProductModel (4, "PHILIPS", 370.1f, "https://images-na.ssl-images-amazon.com/images/I/61tSQahR8vL._AC_SL1277_.jpg"),
+
+       
+
+       };
+       
         public IActionResult Index(int id = -1,string Color = "white")
         {
-            List<ProductModel> Product = new List<ProductModel>();
-            Product.Add(new ProductModel(1, "Espresso Machine", 10.1f, "https://www.ekuep.com/img/cms/Elementor%202/4740-thickbox_default%20(1).jpg"));
-            Product.Add(new ProductModel(2, "Black Coffee", 15.1f, "https://images.indianexpress.com/2015/10/black-coffee_759_petr-kratochvil_wikimedia-commons.jpg"));
-            Product.Add(new ProductModel(3, "Espresso", 17.1f, "https://www.illy.com/content/dam/channels/website/consumer/global/recipies/Doppio-tazza-media_mobile.jpg"));
-
+            
             ViewData["Product"] = Product;
             ViewData["color"] = Color;
             ViewData["id"] = id;
@@ -26,10 +33,7 @@ namespace Shopping.Controllers
         public IActionResult Details(int? id)
         {
 
-            List<ProductModel> Product = new List<ProductModel>();
-            Product.Add(new ProductModel(1, "Espresso Machine", 10.1f, "https://www.ekuep.com/img/cms/Elementor%202/4740-thickbox_default%20(1).jpg"));
-            Product.Add(new ProductModel(2, "Black Coffee", 15.1f, "https://images.indianexpress.com/2015/10/black-coffee_759_petr-kratochvil_wikimedia-commons.jpg"));
-            Product.Add(new ProductModel(3, "Espresso", 17.1f, "https://www.illy.com/content/dam/channels/website/consumer/global/recipies/Doppio-tazza-media_mobile.jpg"));
+            
 
             ProductModel Product1 = Product.Find(b => b.id == id);
 
